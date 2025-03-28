ed24a4684ad2f29521eeb0bbb89bd34f4db2b1ce
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2025-03-28 21:53:54 +0200

    changed

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
+// 1. Анімація тексту
 document.addEventListener('DOMContentLoaded', () => {
-  // Анімація заголовків при завантаженні сторінки
   animateText(document.getElementById("animated-title"), 200);
   animateText(document.getElementById("subtitle"), 100);
 });
-
 function animateText(element, delay) {
-  if (!element) return;
-  
+  if(!element) return;
+
   const text = element.innerText;
   element.innerText = "";
   let index = 0;
@@ -25,24 +24,57 @@ function animateText(element, delay) {
       index++;
       setTimeout(animateNextChar, delay);
     }
-  }
-
-  animateNextChar();
+}
+animateNextChar();
 }
 
+// 2. Завантаження HTML-контенту з іншого файлу
 const loadHTML = (selector, url, callback) => {
   const container = document.querySelector(selector);
-  
-  // Прибираємо кешування, щоб уникнути проблем зі старими даними
   fetch(url)
     .then(response => response.ok ? response.text() : Promise.reject("Error"))
     .then(html => {
       requestAnimationFrame(() => {
-        container.innerHTML = html.replace(/<script.*?<\/script>/gs, ""); // Фікс для видалення всіх скриптів
+        container.innerHTML = html.replace(/<script.*?<\/script>/gs, ""); // Видаляємо скрипти
         callback?.();
       });
     })
     .catch(console.error);
-};
+}
 
+// 3. Закриття меню при скролінгу
+function closeMenu (){
+  document.getElementById('menu-toggle').checked = false;
+}
+window.addEventListener('scroll', function(){
+  if(this.window.scrollY > 0) {
+    closeMenu();
+  }
+});
+
+// 4. Закриття меню при натисканні на посилання в мобільному меню
+const menuLinks = document.querySelectorAll('.mobile-menu-link');
+menuLinks.forEach(link => {
+  link.addEventListener('click', closeMenu);  
+});
+
+// 5. Прокручування сторінки на верх
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+window.addEventListener('load', () => setTimeout(scrollToTop, 0));
+
+// 6. Обробник кліку по логотипу: перезавантаження + прокрутка
+const logo = document.querySelector('#logo');
+logo.addEventListener('click', function (event) {
+  scrollToTop();
+  
+  setTimeout(() => {
+    location.reload(); 
+  }, 500);
+});
 
